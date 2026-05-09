6dff521f39786ac09daad636160e0e407d819002
SV-Mimrox <sv.mimrox@gmail.com>
2026-05-09 16:26:07 +0530

    error resolve 09052026

--- obj/Debug/net8.0/SchoolERP.Net.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchoolERP.Net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolERP.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55568c932b724325ecadb31a28b9779f04502b7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19da6033150763ee26e56640f40eb474eadad45b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolERP.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolERP.Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
